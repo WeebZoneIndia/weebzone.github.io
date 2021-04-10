aecf9241222d21fbbb1cc21f377667e461c9b4ed
ToonsHub <67213740+ToonsHub@users.noreply.github.com>
2021-04-10 21:18:00 +0530

    Update myspecial.js

--- GDrive/myspecial.js
+++ GDrive/myspecial.js
@@ -1,1 +1,1 @@
-document.getElementById("post-body").innerHTML = "You are a big LOL! Visit other sites for downloading cartoons.";
+document.write = "<marquee>You are a big LOL! Visit other sites for downloading cartoons.</marquee>";
