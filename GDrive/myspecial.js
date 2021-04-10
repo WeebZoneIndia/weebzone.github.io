2532356fa65e5290e1883b42a0f868e45a4a95e6
ToonsHub <67213740+ToonsHub@users.noreply.github.com>
2021-04-10 21:21:27 +0530

    Update myspecial.js

--- GDrive/myspecial.js
+++ GDrive/myspecial.js
@@ -1,2 +1,2 @@
-document.write = "<br/><BR/><br/><marquee>You are a big LOL! Visit other sites for downloading cartoons.</marquee>";
-window.StopWhateverBelow();
+document.write = "\<marquee\>You are a big LOL! Visit other sites for downloading cartoons.\<\/marquee\>";
+document.write('<!--');
